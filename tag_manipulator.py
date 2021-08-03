eb8b059c3ca357e0601e8d654d784c2613a09108
unknown <ninasamko@yahoo.com>
2021-08-03 20:36:25 +0100

    cut1 - initial push

---
+++ tag_manipulator.py
@@ -0,0 +1,19 @@
+class TagManipulator():    
+    def parse_string(self, tags):
+        result = []
+
+        if len(tags) < 1 :
+            return result
+
+        return result
+
+
+# class TagManipulator():    
+#     def parse_string(self, tags, regex=""):
+#         result = []
+
+#         tempResult = re.split( regex, tags )
+#         if( len(tempResult[0]) > 0 ):
+#             result = tempResult  
+
+#         return result
