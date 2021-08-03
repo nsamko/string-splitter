f2a9cdbf379b8813e05a37e39213588bdaa99468
unknown <ninasamko@yahoo.com>
2021-08-03 20:50:02 +0100

    cut2 - input single comma, expect empty array

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -14,18 +14,18 @@ def test_split_empty_string_result_empty_array():
     assert result == expResult
 
 
-# def test_split_comma_empty_string_result_empty_array():
-#     # arrange
-#     stringToSplit = ","
-#     expResult = []
-#     result = None
-#     cut = TagManipulator()
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
 
-#     # act
-#     result = cut.parse_string(stringToSplit)
+    # act
+    result = cut.parse_string(stringToSplit)
 
-#     # assert
-#     assert result == expResult
+    # assert
+    assert result == expResult
 
 
 # def test_split_one_string_result_array_of_one():
